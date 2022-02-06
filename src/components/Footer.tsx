d642f064531e36bb27fd6050e77de91672323e84
Ilya Cherkasov <ilya.cherkasov@hqo.co>
2022-02-06 14:28:33 +0300

    Add site for TLT

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -109,6 +109,7 @@ const contacts = [
     city: 'Тольяти',
     phone: '+7 (927) 687 97 50',
     email: 'https://tfdf.ru/',
+    site: 'tfdf.ru',
     instagram: 'https://www.instagram.com/tfdf63/',
     telegram: 'https://t.me/tfdf63',
     vk: 'https://vk.com/tfdf63',
