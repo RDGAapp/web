0f5ebb409fe54a9485afad5d1739b213cc176acc
Ilya Cherkasov <ilya.cherkasov@hqo.co>
2022-04-27 17:59:11 +0300

    Add tests setup and first tests for helpers (#79)
    * Add tests setup and first tests for helpers
    
    * Add eslintignore

---
+++ src/__tests__/helpers/routesList.test.ts
@@ -0,0 +1,22 @@
+import routesList from 'helpers/routesList';
+
+describe('routesList helper', () => {
+  test('should return array of strings', () => {
+    expect(routesList).toEqual([
+      '/',
+      '/about',
+      '/master',
+      '/training',
+      '/newbie',
+      '/pro',
+      '/international',
+      '/shop',
+      '/companies',
+      '/players',
+      '#contacts',
+      '#menu',
+      '#buy',
+      '#join',
+    ]);
+  });
+});
