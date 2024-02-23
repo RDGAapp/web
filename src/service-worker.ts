88ec1b9eb0eba0ef78dc9f5cbe6e02b88cf011ab
Ilya Cherkasov <ilyakopeysk@gmail.com>
2024-02-23 21:29:10 +0300

    remove type from message data

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -126,7 +126,7 @@ registerRoute(
 self.addEventListener('message', (event) => {
   // eslint-disable-next-line no-console
   console.log('SW: message received', event.data);
-  if (event.data?.type === 'skipWaiting') {
+  if (event.data === 'skipWaiting') {
     self.skipWaiting();
   }
 });
