ad2b91b31c3c81d7effb02ea054c920e837f1fbe
Ilya Cherkasov <ilyakopeysk@gmail.com>
2024-02-23 18:34:05 +0300

    disable most of the cache

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,15 +1,15 @@
 /// <reference lib="webworker" />
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { clientsClaim } from 'workbox-core';
-import { ExpirationPlugin } from 'workbox-expiration';
+// import { ExpirationPlugin } from 'workbox-expiration';
 import * as navigationPreload from 'workbox-navigation-preload';
 import { precacheAndRoute } from 'workbox-precaching';
-import { RangeRequestsPlugin } from 'workbox-range-requests';
+// import { RangeRequestsPlugin } from 'workbox-range-requests';
 import { registerRoute } from 'workbox-routing';
 import {
-  CacheFirst,
+  // CacheFirst,
   NetworkFirst,
-  StaleWhileRevalidate,
+  // StaleWhileRevalidate,
 } from 'workbox-strategies';
 
 clientsClaim();
@@ -35,80 +35,80 @@ registerRoute(
 );
 
 // CSS and JS cache
-registerRoute(
-  ({ request }) =>
-    request.destination === 'style' ||
-    request.destination === 'script' ||
-    request.destination === 'worker',
-  new StaleWhileRevalidate({
-    cacheName: 'assets',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-    ],
-  }),
-);
+// registerRoute(
+//   ({ request }) =>
+//     request.destination === 'style' ||
+//     request.destination === 'script' ||
+//     request.destination === 'worker',
+//   new StaleWhileRevalidate({
+//     cacheName: 'assets',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [200],
+//       }),
+//     ],
+//   }),
+// );
 
 // Images cache
-registerRoute(
-  ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'images',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 60 * 60 * 24 * 30,
-      }),
-    ],
-  }),
-);
+// registerRoute(
+//   ({ request }) => request.destination === 'image',
+//   new CacheFirst({
+//     cacheName: 'images',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [200],
+//       }),
+//       new ExpirationPlugin({
+//         maxEntries: 50,
+//         maxAgeSeconds: 60 * 60 * 24 * 30,
+//       }),
+//     ],
+//   }),
+// );
 
 // Video cache
-registerRoute(
-  ({ request }) => request.destination === 'video',
-  new CacheFirst({
-    cacheName: 'videos',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 10,
-        maxAgeSeconds: 60 * 60 * 24 * 30,
-      }),
-      new RangeRequestsPlugin(),
-    ],
-  }),
-);
+// registerRoute(
+//   ({ request }) => request.destination === 'video',
+//   new CacheFirst({
+//     cacheName: 'videos',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [200],
+//       }),
+//       new ExpirationPlugin({
+//         maxEntries: 10,
+//         maxAgeSeconds: 60 * 60 * 24 * 30,
+//       }),
+//       new RangeRequestsPlugin(),
+//     ],
+//   }),
+// );
 
 // Google stylesheets cache
-registerRoute(
-  ({ url }) => url.origin === 'https://fonts.googleapis.com',
-  new StaleWhileRevalidate({
-    cacheName: 'google-fonts-stylesheets',
-  }),
-);
+// registerRoute(
+//   ({ url }) => url.origin === 'https://fonts.googleapis.com',
+//   new StaleWhileRevalidate({
+//     cacheName: 'google-fonts-stylesheets',
+//   }),
+// );
 
 // Google fonts cache
-registerRoute(
-  ({ url }) => url.origin === 'https://fonts.gstatic.com',
-  new CacheFirst({
-    cacheName: 'google-fonts-webfonts',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-        maxEntries: 10,
-      }),
-    ],
-  }),
-);
+// registerRoute(
+//   ({ url }) => url.origin === 'https://fonts.gstatic.com',
+//   new CacheFirst({
+//     cacheName: 'google-fonts-webfonts',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//       new ExpirationPlugin({
+//         maxAgeSeconds: 60 * 60 * 24 * 365,
+//         maxEntries: 10,
+//       }),
+//     ],
+//   }),
+// );
 
 // API responses
 registerRoute(
