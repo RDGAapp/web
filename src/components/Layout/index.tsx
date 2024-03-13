bd193bc03729f47890cb02ea40de7afd32857272
Ilya Cherkasov <ilyakopeysk@gmail.com>
2024-03-14 01:18:23 +0300

    remove helmet

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,10 @@
 import { Suspense, useContext, useLayoutEffect } from 'react';
 
-import { Helmet } from 'react-helmet';
 import { Outlet, useLocation } from 'react-router';
 import { ScrollRestoration } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import styled from 'styled-components';
 
-import VideoPreview from 'assets/images/preview.webp';
-import VideoWebm from 'assets/videos/banner.webm';
 import { AppSettingsContext } from 'context/AppSettings';
 import Role from 'enums/roles';
 import routes from 'helpers/routes';
@@ -73,132 +70,114 @@ const Layout = (): JSX.Element => {
   }, [openFeatureFlagsModal]);
 
   return (
-    <>
-      <Helmet>
-        <meta property='og:title' content='Российская Диск-Гольф Ассоциация' />
-        <meta
-          property='og:description'
-          content='Российская Диск-Гольф Ассоциация - объединение игроков в диск-гольф, целью которого является развитие этого спорта в России.'
-        />
-        <meta property='og:image' content={VideoPreview} />
-        <meta property='og:video' content={VideoWebm} />
-        <meta property='og:locale' content='ru_RU' />
-        <meta
-          property='og:site_name'
-          content='Российская Диск-Гольф Ассоциация'
-        />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://rdga.ru' />
-      </Helmet>
-      <div
-        style={{
-          minHeight: '100dvh',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <SWNotification />
-        <Header />
-        <Container>
-          {shouldShowVideo.has(location.pathname) && <VideoBanner />}
-          {shouldShowBanner.has(location.pathname) && <Banner />}
-          <Suspense fallback={<Loading />}>
-            <Outlet />
-          </Suspense>
-        </Container>
-        <Footer />
-        <FeatureFlagsDialog>
+    <div
+      style={{
+        minHeight: '100dvh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <SWNotification />
+      <Header />
+      <Container>
+        {shouldShowVideo.has(location.pathname) && <VideoBanner />}
+        {shouldShowBanner.has(location.pathname) && <Banner />}
+        <Suspense fallback={<Loading />}>
+          <Outlet />
+        </Suspense>
+      </Container>
+      <Footer />
+      <FeatureFlagsDialog>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '0.5rem',
+            overflow: 'hidden',
+          }}
+        >
           <div
             style={{
               display: 'flex',
-              flexDirection: 'column',
-              gap: '0.5rem',
-              overflow: 'hidden',
+              gap: '10px',
+              alignItems: 'center',
+              cursor: 'pointer',
             }}
           >
-            <div
-              style={{
-                display: 'flex',
-                gap: '10px',
-                alignItems: 'center',
-                cursor: 'pointer',
-              }}
-            >
-              <input
-                id='isAdmin'
-                type='checkbox'
-                name='isAdmin'
-                checked={isAdmin}
-                onChange={() =>
-                  isAdmin ? removeRole(Role.Admin) : addRoles([Role.Admin])
-                }
-                style={{ cursor: 'pointer' }}
-              />
-              <label htmlFor='isAdmin' style={{ cursor: 'pointer' }}>
-                Режим администратора
-              </label>
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                gap: '10px',
-                alignItems: 'center',
-                cursor: 'pointer',
-              }}
-            >
-              <input
-                id='isAuthor'
-                type='checkbox'
-                name='isAuthor'
-                checked={isAuthor}
-                onChange={() =>
-                  isAuthor ? removeRole(Role.Author) : addRoles([Role.Author])
-                }
-                style={{ cursor: 'pointer' }}
-              />
-              <label htmlFor='isAuthor' style={{ cursor: 'pointer' }}>
-                Режим автора
-              </label>
-            </div>
-            {Object.keys(featureFlags).map((key) => (
-              <div
-                key={key}
-                style={{
-                  display: 'flex',
-                  gap: '10px',
-                  alignItems: 'center',
-                  cursor: 'pointer',
-                }}
-              >
-                <input
-                  id={key}
-                  type='checkbox'
-                  name={key}
-                  checked={featureFlags[key as keyof typeof featureFlags]}
-                  onChange={() =>
-                    toggleFeatureFlag(key as keyof typeof featureFlags)
-                  }
-                  style={{ cursor: 'pointer' }}
-                />
-                <label htmlFor={key} style={{ cursor: 'pointer' }}>
-                  {key}
-                </label>
-              </div>
-            ))}
+            <input
+              id='isAdmin'
+              type='checkbox'
+              name='isAdmin'
+              checked={isAdmin}
+              onChange={() =>
+                isAdmin ? removeRole(Role.Admin) : addRoles([Role.Admin])
+              }
+              style={{ cursor: 'pointer' }}
+            />
+            <label htmlFor='isAdmin' style={{ cursor: 'pointer' }}>
+              Режим администратора
+            </label>
           </div>
-        </FeatureFlagsDialog>
-        <ScrollRestoration />
-        <ToastContainer
-          position='bottom-right'
-          hideProgressBar
-          closeOnClick
-          theme='colored'
-          newestOnTop
-          stacked
-          style={{ fontSize: '0.8rem' }}
-        />
-      </div>
-    </>
+          <div
+            style={{
+              display: 'flex',
+              gap: '10px',
+              alignItems: 'center',
+              cursor: 'pointer',
+            }}
+          >
+            <input
+              id='isAuthor'
+              type='checkbox'
+              name='isAuthor'
+              checked={isAuthor}
+              onChange={() =>
+                isAuthor ? removeRole(Role.Author) : addRoles([Role.Author])
+              }
+              style={{ cursor: 'pointer' }}
+            />
+            <label htmlFor='isAuthor' style={{ cursor: 'pointer' }}>
+              Режим автора
+            </label>
+          </div>
+          {Object.keys(featureFlags).map((key) => (
+            <div
+              key={key}
+              style={{
+                display: 'flex',
+                gap: '10px',
+                alignItems: 'center',
+                cursor: 'pointer',
+              }}
+            >
+              <input
+                id={key}
+                type='checkbox'
+                name={key}
+                checked={featureFlags[key as keyof typeof featureFlags]}
+                onChange={() =>
+                  toggleFeatureFlag(key as keyof typeof featureFlags)
+                }
+                style={{ cursor: 'pointer' }}
+              />
+              <label htmlFor={key} style={{ cursor: 'pointer' }}>
+                {key}
+              </label>
+            </div>
+          ))}
+        </div>
+      </FeatureFlagsDialog>
+      <ScrollRestoration />
+      <ToastContainer
+        position='bottom-right'
+        hideProgressBar
+        closeOnClick
+        theme='colored'
+        newestOnTop
+        stacked
+        style={{ fontSize: '0.8rem' }}
+      />
+    </div>
   );
 };
 
