8c2a131999c43139843b00222e0d074256998bd2
Ilya Cherkasov <ilyakopeysk@gmail.com>
2024-03-18 19:36:38 +0300

    move setup file

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['src/setUpTests.ts'],
+    setupFiles: ['./vitest.setup.ts'],
     include: ['**/*.test.ts?(x)'],
   },
 });
