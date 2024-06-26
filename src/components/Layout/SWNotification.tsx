6b55d367b10e2b36e10e14908db8b20ab416d742
Ilya Cherkasov <ilyakopeysk@gmail.com>
2024-06-26 22:28:41 +0300

    Layout. Less agressive update prompt + update packages

--- src/components/Layout/SWNotification.tsx
+++ src/components/Layout/SWNotification.tsx
@@ -1,36 +1,7 @@
-import { CSSProperties } from 'react';
+import { useEffect } from 'react';
 
-import styled from 'styled-components';
+import { toast } from 'react-toastify';
 import { useRegisterSW } from 'virtual:pwa-register/react';
-
-const Container = styled.div`
-  cursor: pointer;
-
-  position: fixed;
-  z-index: ${({ theme }) => theme.zIndex.swNotification};
-  top: -10rem;
-  left: 50%;
-  transform: translateX(-50%);
-
-  width: max-content;
-  max-width: 80vw;
-  padding: 0.5rem 1.5rem;
-
-  font-size: 1rem;
-  color: ${({ theme }) => theme.colors.black};
-  text-align: center;
-
-  opacity: 0;
-  background-color: ${({ theme }) => theme.colors.additional};
-  border-radius: 2rem;
-
-  transition: all 1s ease-in-out;
-`;
-
-const showStyles: CSSProperties = {
-  top: '1.5rem',
-  opacity: 1,
-};
 
 const SWNotification = () => {
   const {
@@ -42,22 +13,21 @@ const SWNotification = () => {
     },
   });
 
-  return (
-    <Container
-      id='reloadNotification'
-      style={needRefresh ? showStyles : {}}
-      onClick={
-        needRefresh
-          ? () => {
-              updateServiceWorker(true);
-              setNeedRefresh(false);
-            }
-          : undefined
-      }
-    >
-      Доступна более новая версия сайта! Нажмите для обновления.
-    </Container>
-  );
+  useEffect(() => {
+    if (!needRefresh) return;
+
+    toast.error('Доступна более новая версия сайта! Нажмите для обновления.', {
+      onClick: () => {
+        updateServiceWorker(true);
+        setNeedRefresh(false);
+      },
+      autoClose: false,
+      closeButton: false,
+      closeOnClick: true,
+    });
+  }, [needRefresh]);
+
+  return null;
 };
 
 export default SWNotification;
